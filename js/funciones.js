e3f9b4903e4e6b35bb68f8ddd08223d017330373
ZackDream <150211062+ZackDream@users.noreply.github.com>
2023-11-07 21:25:04 -0600

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,34 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const close = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () =>{
+
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () =>{
+
+    nav.classList.remove("visible");
+}) 
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_nq7cpr8';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
