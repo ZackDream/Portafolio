736ebd2ccccd6fa6116cff448b5414e2035644dd
ZackDream <150211062+ZackDream@users.noreply.github.com>
2023-11-20 22:59:45 -0600

    Add files via upload

--- js/funciones.js
+++ js/funciones.js
@@ -32,3 +32,17 @@ document.getElementById('form')
       alert(JSON.stringify(err));
     });
 });
+
+var header = document.getElementById('Header')
+
+window.addEventListener("scroll", function(){
+    var scroll = window.scrollY;
+    if(scrollY>0){
+        header.style.backgroundColor = '#121212';
+
+    }
+    else{
+        header.style.backgroundColor = 'transparent';
+    }
+
+})
